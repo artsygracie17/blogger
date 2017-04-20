a3815f9ad2ccd59c82d336d88554ba6c67dc1be1
Gracie Liu-Fang <gracie.fang@veracross.com>
2017-04-20 17:07:14 -0400

    finished blog comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   #get "/something" => "articles#index", as: "cupcake" #controller, hash, action (articles#index) cupcake is /something
   root to: 'articles#index'
 end
