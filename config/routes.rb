59e8b588ae9104f437e2158cba8c41c119307811
Gracie Liu-Fang <gracie.fang@veracross.com>
2017-04-20 11:21:25 -0400

    form-based workflow feature complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :articles
+  #get "/something" => "articles#index", as: "cupcake" #controller, hash, action (articles#index) cupcake is /something
+  root to: 'articles#index'
 end
