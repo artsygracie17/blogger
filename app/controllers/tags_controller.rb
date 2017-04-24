65f4b7881633fa374cfe7dec8a2477629a9e6115
Gracie Liu-Fang <gracie.fang@veracross.com>
2017-04-24 12:25:33 -0400

    tagging feature completed

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,16 @@
+class TagsController < ApplicationController
+  def show
+    @tag = Tag.find(params[:id])
+  end
+  def index
+    @tags = Tag.all
+  end
+  def destroy
+    tag = Tag.find(params[:id])
+    tag_name = tag.name
+    tag.destroy
+    flash.notice = "Tag '#{tag_name}' deleted!"
+    redirect_to tags_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
-Rails.application.routes.draw do
+Blogger::Application.routes.draw do
+
+  root to: 'articles#index'
   resources :articles do
     resources :comments
   end
+  resources :tags
   #get "/something" => "articles#index", as: "cupcake" #controller, hash, action (articles#index) cupcake is /something
-  root to: 'articles#index'
 end
